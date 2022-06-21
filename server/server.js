eca126f27df733f1e7017f6d03a3c67f7d56acc7
superfishal <97133716+superfishal@users.noreply.github.com>
2022-06-20 17:40:02 -0700

    proxy setup to run client and server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 // authentication middleware for JWT's
 const { authMiddleware } = require("./utils/auth");
-
+const path = require("path");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -20,7 +20,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
